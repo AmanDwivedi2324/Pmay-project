0bb71587942bf9f76bbaf2b7502589efddd47b4f
AmanDwivedi2324 <0786ad2000@gmail.com>
2025-10-28 20:55:28 +0530

    First commit

---
+++ src/test/java/com/aparsh/api/pmay/PmayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aparsh.api.pmay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
